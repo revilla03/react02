1e81b8f0390d8056433ae88d3a65a152c8492556
carol <carolrevilla0305@.com>
2023-10-18 18:11:18 -0500

    v5 creacion de form,c04nuevovomponente y firebase

---
+++ src/components/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmVme0D5urCe982qqc_TdfXacnf1Nhpmk",
+  authDomain: "appreact-b374a.firebaseapp.com",
+  projectId: "appreact-b374a",
+  storageBucket: "appreact-b374a.appspot.com",
+  messagingSenderId: "913390551570",
+  appId: "1:913390551570:web:d6dae0e41e61fb41548ecb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
