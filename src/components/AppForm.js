31b77f0918130cc384ef089468ae2b8e17ec5d7f
carol <carolrevilla@gmail.com>
2023-10-24 19:36:55 -0500

    v6 actualizacion de fire base,enlace de base de datos,se edito fire base y appform

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from './firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 const AppForm = () => {
+
+  ////////////////GUARDAR O ACTUALIZAR////////////////
+  const camposRegistro = { nombre: "", edad:"",genero:""}
+  const [objeto, setObjeto] = useState(camposRegistro);
+  const handleSubmit  =(e)=> { //manejador de submit
+    e.preventDefault();
+
+    try {
+      if(db){
+        addDoc (collection(db, 'cliente'), objeto);
+        console.log("se guardo con exito"); 
+      }
+    }catch (error){
+      console.error();
+    }
+  }
   return (
     <div style ={{background:"orange",padding:"10px", textAlign:"center"}}>
+      <form onSubmit={ handleSubmit }>
       <h1>AppForm.js</h1>
-      <p>Nombre</p>
-      <p>Edad</p>
-      <p>Genero</p>
-      <p>Guardar</p>
+      <input type='text'placeholder='Nombre...'/>
+      <input type='text'placeholder='Apellido...'/>
+      <select>
+        <option value="">Seleccione...</option>
+        <option value="M">Masculino...</option>
+        <option value="F">Femenino...</option>
+        </select>
+        <button>
+          GUARDAR/ACTUALIZAR
+        </button>
+      
+      </form>
     </div>
   )
 }
