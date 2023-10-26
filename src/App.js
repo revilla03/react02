715685620e1f08db6290e07260f87c6c598a63c4
carol <carolrevilla0305@gmail.com>
2023-10-25 19:29:56 -0500

    v7 validacion, lectura BD

--- src/App.js
+++ src/App.js
@@ -3,49 +3,55 @@ import AppForm from './components/AppForm';
 import logo from './logo.svg';
 //import './App.css';
 import C01componente from './pagina/C01componente';
-import { collection, doc, onSnapshot, query } from 'firebase/firestore';
+import { collection, onSnapshot, query } from 'firebase/firestore';
 import { db } from './components/firebase';
 
-function App() {
 
-  ////////////READ-LECTURA -fnRead/////////
-  const[docBD,seDocBD]=useState([]);
-  const fnRead =()=> {
-    try {
-      const xColeccionConQuery = query (collection(db,"persona"));
-      const unsubcribe = onSnapshot(xColeccionConQuery,(xDatosBD)=> {
-        const xDoc = [];
-        xDatosBD.forEach ((doc)=> {
-          xDoc.push({id:doc.id,...doc.data()});
-        });
+function App() {
+  ///////////READ - LECTURA - FNREAD//////////
+  const[docBD, setDocBD] = useState([]);
+  const fnRead = () => {
+      try {
+        const xColeccionConQuery = query(collection(db, 'persona'));
+        const unsubcribe = onSnapshot(xColeccionConQuery, (xDatosBD) => {
+            const xDoc = [];
+            xDatosBD.forEach( (doc) => {
+             xDoc.push({id: doc.id, ...doc.data()});
+            });
+            setDocBD(xDoc);
+          
+          });   
+      } catch (error) {
+        console.error(error);
         
-        seDocBD(xDoc);
-      });
-    } catch (error) {
-      console.error(error);
-    }
+      }
+  
+
   }
-  /////////// DELETE -Eliminar-fnDelete/////
-  const [idActual, setIdActual]= useState("");
-  const fnDelete = (xId)=> {}
+
+  fnRead();
+    ////////// DELETE - ELEMINAR - FNDELETE//////
+    const [idActual, setIdActual] = useState ("");
+    const fnDelete = (xId) => {}
+
+  
+ 
 
   return (
-    <div style ={{width:"350px",background:"greenyellow",padding:"10px"}}>
-      <AppForm {...{idActual, setIdActual, fnRead}} />
-      {docBD.map ((p) =>
-      <p key={p.id}>
-      No. 1 {p.nombre}...
-      <span onClick={() => fnDelete(p.id)}>x </span>
-      ...
-      <span onClick={() => setIdActual(p.id)}> A </span>
-      </p>
+    <div style={{ width: "350px", background: "greenyellow", padding: "10px" }}>
+      <AppForm {...{ idActual, setIdActual, fnRead }} />
+      { 
+        docBD.map((p) =>
+          <p key={p.id}> 
+            No. {p.nombre}...
+            <span onClick={() => fnDelete(p.id)}>X</span>
+            .....
+            <span onClick={() => setIdActual(p.id)}>A</span>
+          </p>
         )
       }
-    
-  
+     
     </div>
-    
   );
 }
-
 export default App;
